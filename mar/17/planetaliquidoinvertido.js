b45bfa93fc5c6c1e14f3bb97f18df99c9817af1a
maca-quuack <macarena.villa@quuack.com>
2022-03-17 11:41:19 -0300

    planeta liquido invertido

---
+++ mar/17/planetaliquidoinvertido.js
@@ -0,0 +1,17 @@
+solid(1, 1, 1)
+.diff(
+  shape(99, .451).scale(1, 1, ()=>window.innerWidth / window.innerHeight )
+  .mask(
+    shape(1)
+    .scrollY(0.3)
+    .modulateScale(
+      osc(5, .4).rotate(0, 0.5 )
+    )
+    .modulateRotate(
+      osc(5, .4).rotate(0, 0.5 )
+    )
+    .scale(.25)
+  )
+
+)
+.out()
