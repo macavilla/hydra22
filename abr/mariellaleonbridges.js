2a9d695423fdde59f29cf80bc42eabcc398e521c
maca-quuack <macarena.villa@quuack.com>
2022-04-08 15:18:55 -0300

    nuevp mariella

--- abr/mariellaleonbridges.js
+++ abr/mariellaleonbridges.js
@@ -1,22 +1,44 @@
 let globe = (size) =>  shape(99,size,.004).scale(1,1,()=>window.innerWidth/window.innerHeight);
 
-osc(40, 0.01, .0).rotate(1.57).posterize(2.5)
+//cielo
+//osc(40, .05, .0).rotate(1.57).posterize(2.5)
+osc(2, .05).rotate(1.57).posterize(9)
+
+//estrellas
+.add(
+  shape(4, .12).rotate(.59).repeat(20, 10).modulate(noise(2))
+  .add(
+    shape(99, .12).repeat(16, 13).modulate(osc(-1))
+  )
+  .mask(
+    shape(2, .1, .5).scrollY(.4)
+  )
+  //.modulate(osc(1, .25, .6).modulate(noise(15)))
+)
 .mask(
-  shape(2, .5).scrollY(.24)
+  shape(2, .5).scrollY(.2)
 ).add(
-  globe(.599999).sub(globe(.59)).add(globe(.58))
+  //luna
+  globe(.599999).sub(globe(.59)).add(globe(.58)).scale(.789)
+
+  .scrollY(0, .0245)
 , 1)
 
+//suelo
 .layer(
-  osc(2, -.1).color(.4, 0, .6).saturate(.4).rotate(1.57).posterize(9)
-  .modulate(shape(3))
+  //osc(2, -.05).rotate(1.57).posterize(9)
+  osc(40, -.05, .0).rotate(1.57).posterize(2.5)
+
   .mask(shape(2, .5)).scrollY(-.25)
 )
+//ruta
 .add(
-  shape(3, .3, 0).rotate(1.05).scrollY(-.309).scale(1.4, 2, 1)
-  .sub(shape(3, .29, 0).rotate(1.05).scrollY(-.309).scale(1.4, 2, 1))
+  shape(3, .3, 0).rotate(1.05).scrollY(-.29).scale(1.4, 2, 1)
+  .sub(shape(3, .29, 0).rotate(1.05).scrollY(-.29).scale(1.4, 2, 1))
   .add(
-    shape(3, .29, 0).color(.01, .01, .01).rotate(1.05).scrollY(-.309).scale(1.4, 2, 1)
-  ).sub(shape(3, .27, 0).rotate(1.05).scrollY(-.309).scale(1.4, 2, 1))
+    shape(3, .29, 0).color(.01, .01, .01).rotate(1.05).scrollY(-.25).scale(1.4, 2, 1)
+  ).sub(shape(3, .27, 0).rotate(1.05).scrollY(-.275).scale(1.4, 2, 1))
+
 )
+
 .out()
