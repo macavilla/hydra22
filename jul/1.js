2d4919f118d4b5173db60ce99800ea83f53170eb
macavilla <maca.villa.a@gmail.com>
2022-08-21 23:13:32 -0300

    add hydra zine draft

--- jul/1.js
+++ jul/1.js
@@ -1,16 +1,16 @@
 osc(2, .1, [.5, .4, .7, .9, 1, 1.25].smooth()).posterize(5)
-.modulateRotate(
+.modulate(
   noise(1)
 )
 .color(.9, .4, .7)
 .add(
-  shape(99, .53, 0).scale(.71, 1.5, ()=>window.innerWidth/window.innerHeight)
-  .mult(
+  shape(3, [.3, .5, .9].smooth(), 0).scale(.71, 1.5, ()=>window.innerWidth/window.innerHeight)
+  .sub(
     src(o0)
   )
   //.color(.51, .1, .91)
   .modulateScale(
-    noise(2, .02)
+    noise(2, .2)
   )
 )
 //.invert([.7, 1].smooth().fast(.2))
